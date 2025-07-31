ac84ee3871079f77fe675bb0fb9e0949e59b2d88
igorpandolf <pandolf.igor@gmail.com>
2025-07-30 21:11:52 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data // Cria os Getters e Setters
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -18,37 +24,4 @@ public class MissoesModel {
     // @OneToMany - Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, RankMissao rank) {
-        this.id = id;
-        this.nome = nome;
-        this.rank = rank;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeMissao() {
-        return nome;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nome = nome;
-    }
-
-    public RankMissao getRank() {
-        return rank;
-    }
-
-    public void setRank(RankMissao rank) {
-        this.rank = rank;
-    }
 }
