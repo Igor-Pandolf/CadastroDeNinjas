7437157a580a8e1527fbd3f703a1f5625af01de2
igorpandolf <pandolf.igor@gmail.com>
2025-08-11 20:52:52 -0300

    fix: Inclui a anotation @JsonIgnore para previnir o erro de serialização loop infinito

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -26,5 +27,6 @@ public class MissoesModel {
 
     // @OneToMany - Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore // Vai ignorar a serialização, impedindo o loop infinito
     private List<NinjaModel> ninja;
 }
